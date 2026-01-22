de5efb06cae7659d3f4c92f5e3049ad19408d4fc
rickyhussein <94778902+rickyhussein@users.noreply.github.com>
2026-01-22 21:16:17 +0700

    update

--- config/midtrans.php
+++ config/midtrans.php
@@ -6,9 +6,7 @@ return [
     'server_key' => env('MIDTRANS_SERVER_KEY'),
     'is_production' => env('MIDTRANS_IS_PRODUCTION'),
     'snap_url' => env('MIDTRANS_SNAP_URL'),
-    'whatsapp_api_url' => env('MIDTRANS_WHATSAPP_API_URL'),
-    'whatsapp_api_key' => env('MIDTRANS_WHATSAPP_API_KEY'),
-    'whatsapp_sender' => env('MIDTRANS_WHATSAPP_SENDER'),
-    'railway_whatsapp_api_url' => env('RAILWAY_WHATSAPP_API_URL'),
-    'railway_whatsapp_api_session' => env('RAILWAY_WHATSAPP_API_SESSION'),
+    'whatsapp_api_url' => env('WHATSAPP_API_URL'),
+    'whatsapp_api_session' => env('WHATSAPP_API_SESSION'),
+    'whatsapp_api_key' => env('WHATSAPP_API_KEY'),
 ];
